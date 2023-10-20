f88115b09b06fe18ef7a0ddd93d9957b6d6caca4
SAHANA-POOJARY <105369167+SAHANA-POOJARY@users.noreply.github.com>
2023-10-20 18:19:42 +0530

    Add files via upload

---
+++ Register/src/test/java/com/lulu/Register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulu.Register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
